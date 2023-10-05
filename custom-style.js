fe8ad999764fd2c20c10efb1e7700bda27aa2bc8
Ashiqweb123 <rabiulazim430@gmail.com>
2023-10-04 23:36:57 -0700

    clint-reviw add

---
+++ custom-style.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          "bike-color": '#E76F51',
+          "banner-color": 'rgba(231, 111, 81, 0.10)',
+        }
+      }
+    }
+  }
